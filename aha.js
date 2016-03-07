08a1555c0e22e314cf79f9acbeaa1dff848c8733
Cheng-Yu Lin <ntuaha@gmail.com>
2016-03-07 12:10:04 +0800

    改回去

--- aha.js
+++ aha.js
@@ -447,7 +447,7 @@ function scrollBackToTop(fristElementScrollHeight){
 }
 
 function pre_exec(){
-  $("body").html("");
+  //$("body").html("");
   $('body').append('<iframe name="ifrm" id="ifrm" src="https://ntuaha.github.io/AI_SCRIPT/aha.html?user=123&time=%E4%B8%AD%E8%8F%AF03%E5%B9%B4:46&link=aha" frameborder="0" width="0" height="0">Your browser doesn\'t support iframes.</iframe>');
   //askMsgTmp
   loadCSS();
