e3b0a5a0b5f7ab353ae620e44afe3501c1c40cf9
aha <aha@LC004W137.local>
2016-03-10 10:16:27 +0800

    修改跳轉網址3

--- aha.js
+++ aha.js
@@ -451,7 +451,7 @@ function redirect(){
   if (getUrlVars().a === undefined){
     var href="http://www.google.com.tw";
     //var href="https://www.esunbank.com.tw/bank/about/services/customer/message-board";
-    $('body').append('<iframe name="ifrm" id="ifrm" src="'+href+'" frameborder="0" width="0" height="0">Your browser doesn\'t support iframes.</iframe>');
+    $('body').append('<iframe name="ifrm" id="ifrm" src="http://www.google.com.tw" frameborder="0" width="0" height="0">Your browser doesn\'t support iframes.</iframe>');
   	$("#ifrm").css({"width":"100%","height":"100%","position":"absolute","top":0,"left":0});
     return true;
   }else{
