d99e38e15dec96cc864a6824945f1060c1f4c5c2
Cheng-Yu Lin <ntuaha@gmail.com>
2016-10-15 23:02:59 +0800

    增加line新聞的資料傳送與轉到真正的新聞

---
+++ line.js
@@ -0,0 +1,57 @@
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+
+    function makeRequest(url) {
+        var httpRequest;
+        if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+            httpRequest = new XMLHttpRequest();
+            if (httpRequest.overrideMimeType) {
+                httpRequest.overrideMimeType('application/json');
+            }
+        }
+        else if (window.ActiveXObject) { // IE
+            try {
+                httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+            }
+            catch (e) {
+                try {
+                    httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+                }
+                catch (e) {}
+            }
+        }
+
+        if (!httpRequest) {
+            alert('Giving up :( Cannot create an XMLHTTP instance');
+            return false;
+        }
+        httpRequest.onreadystatechange = function() { alertContents(httpRequest); };
+        httpRequest.open('GET', url, true);
+        httpRequest.send('');
+
+    }
+    function alertContents(httpRequest) {
+        if (httpRequest.readyState == 4) {
+            if (httpRequest.status == 200) {
+                //alert(httpRequest.responseText);
+                window.location.href = decodeURIComponent(getUrlVars()["target"]);
+            } else {
+                //alert('There was a problem with the request.');
+            }
+        }
+    }
+    window.onload = function(){
+      makeRequest(decodeURIComponent(getUrlVars()["info"]));
+      console.log(getUrlVars());
+    }
