df1c76bf5ffbd745d04e5cc2db98ca11ccb5b031
Cheng-Yu Lin <ntuaha@gmail.com>
2016-10-16 00:56:57 +0800

    remove useless comment

--- line.js
+++ line.js
@@ -10,66 +10,15 @@ function getUrlVars()
     }
     return vars;
 }
-/*
 
-    function makeRequest(url) {
-        var httpRequest;
-        if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-            httpRequest = new XMLHttpRequest();
-            if (httpRequest.overrideMimeType) {
-                httpRequest.overrideMimeType('application/json');
-            }
-        }
-        else if (window.ActiveXObject) { // IE
-            try {
-                httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
-            }
-            catch (e) {
-                try {
-                    httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
-                }
-                catch (e) {}
-            }
-        }
-
-        if (!httpRequest) {
-            alert('Giving up :( Cannot create an XMLHTTP instance');
-            return false;
-        }
-        httpRequest.onreadystatechange = function() { alertContents(httpRequest); };
-        httpRequest.open('GET', url, true);
-        httpRequest.send('');
-
-    }
-    function alertContents(httpRequest) {
-        if (httpRequest.readyState == 4) {
-            if (httpRequest.status == 200) {
-                //alert(httpRequest.responseText);
-                window.location.href = decodeURIComponent(getUrlVars()["target"]);
-            } else {
-                //alert('There was a problem with the request.');
-            }
-        }
-    }
-
-    var script = document.createElement('script');
-    script.src = "http://jsonp.guffa.com/Proxy.ashx?url="+getUrlVars()["info"];
-    //document.getElementsByTagName('head')[0].appendChild(script);
-
-    window.onload = function(){
+$(function(){
+  $.ajax({
+    type: 'GET',
+    url: decodeURIComponent(getUrlVars()["info"]),
+    dataType: 'script',
+    success: function (data) {
       window.location.href = decodeURIComponent(getUrlVars()["target"]);
-      //makeRequest();      
-      console.log(getUrlVars());
     }
-*/
-    $(function(){
-      $.ajax({
-        type: 'GET',
-        url: decodeURIComponent(getUrlVars()["info"]),
-        dataType: 'script',
-        success: function (data) {
-          window.location.href = decodeURIComponent(getUrlVars()["target"]);
-        }
-      });
-      
-    });
+  });
+  
+});
